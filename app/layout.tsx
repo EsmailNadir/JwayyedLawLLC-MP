077038b970449a8506a9ceb7efc998deb6dfec95
EsmailNadir <112122818+EsmailNadir@users.noreply.github.com>
2025-05-31 23:22:25 -0700

    Set up Tailwind and initial homepage layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import Navbar from "@/components/navbar";
+import "./globals.css";
+
+
 export const metadata = {
   title: 'Jwayyed Law LLC',
   description: 'Your trusted legal advisor',
@@ -6,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Navbar />
+
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
+  export default function Home() {
+    return (
+     
     <div>
-      <h1>
-        Jwayyed law llc 
-      </h1>
+      
     </div>
-  );
-}
+
+    );
+  }
+  
