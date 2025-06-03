0e498cbec48eb4f1c58177209132606f322320a8
EsmailMazin <99042650+EsmailMazin@users.noreply.github.com>
2025-06-03 00:37:07 -0700

    Updated layout.tsx and updated video file

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <Head>
         <script
           type="text/javascript"
@@ -20,10 +20,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           async
         ></script>
       </Head>
-      <body>
-      <Navbar />
-        {children}
-        <Footer/>
+      <body className="h-full flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
